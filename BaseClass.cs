c8ba65191d3c1457c17cee03911830c8c703fecc
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-10-08 10:02:12 +0200

    BaseClass and Country documentation tags

--- BaseClass.cs
+++ BaseClass.cs
@@ -4,9 +4,28 @@ namespace Amica.vNext.Objects
 {
     public abstract class BaseClass
     {
+		/// <summary>
+		/// Gets or sets the unique Id.
+		/// </summary>
+		/// <value>The unique identifier.</value>
         public string Id { get; set; }
+
+		/// <summary>
+		/// Gets or sets the ETag.
+		/// </summary>
+		/// <value>The ETag.</value>
         public string ETag { get; set; }
+
+		/// <summary>
+		/// Gets or sets the last update date and time.
+		/// </summary>
+		/// <value>The last updated date and time.</value>
         public DateTime Updated { get; set; }
+
+		/// <summary>
+		/// Gets or sets the creation date.
+		/// </summary>
+		/// <value>The creation date.</value>
         public DateTime Created { get; set; }
     }
 }
--- Country.cs
+++ Country.cs
@@ -2,6 +2,10 @@
 {
     public class Country : BaseClass
     {
+		/// <summary>
+		/// Gets or sets the country name.
+		/// </summary>
+		/// <value>The country name.</value>
         public string Name { get; set; }
     }
 }
