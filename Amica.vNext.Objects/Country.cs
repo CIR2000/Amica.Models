ece50f1206b63ea6f02754006d9240abd3a8d855
Nicola Iarocci <nicola@nicolaiarocci.com>
2014-11-11 16:28:03 +0100

    Map NazioniRow.Name to countries.n

--- Amica.vNext.Objects/Country.cs
+++ Amica.vNext.Objects/Country.cs
@@ -1,4 +1,6 @@
-﻿namespace Amica.vNext.Objects
+﻿using Newtonsoft.Json;
+
+namespace Amica.vNext.Objects
 {
     public class Country : BaseClass
     {
@@ -6,6 +8,7 @@
         /// Gets or sets the country name.
         /// </summary>
         /// <value>The country name.</value>
+        [JsonProperty("n")]
         public string Name { get; set; }
     }
 }
