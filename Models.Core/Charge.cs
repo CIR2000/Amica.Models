5e1baecbd0d825199600fed41fdd8a71e9ede3ca
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-03-21 16:21:16 +0100

    Add Charge model

---
+++ Models.Core/Charge.cs
@@ -0,0 +1,35 @@
+﻿namespace Amica.vNext.Models
+{
+    public class Charge : BaseModelWithCompanyId
+    {
+        private string _name;
+        private decimal _amount;
+        private Vat _vat = new Vat();
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            set { SetProperty(ref _name, value); }
+            get { return _name; }
+        }
+
+        /// <summary>
+        /// Gets or sets wether this is a RiBa method.
+        /// </summary>
+        /// <value>The wether this is a RiBa method.</value>
+        public decimal Amount
+        {
+            set { SetProperty(ref _amount, value); }
+            get { return _amount; }
+        }
+
+        public Vat Vat
+        {
+            set { SetProperty(ref _vat, value); }
+            get { return _vat; }
+        }
+    }
+}
