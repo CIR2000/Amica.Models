910d714308bf74049be6fbeaf8100af3063a841e
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-04-07 09:26:46 +0200

    Add Document.SocialSecurity class model

---
+++ Models.Core/Document/SocialSecurity.cs
@@ -0,0 +1,28 @@
+﻿namespace Amica.vNext.Models.Documents
+{
+    public class SocialSecurity : ObservableObject
+    {
+		private string _name;
+        private double _rate;
+        private decimal _amount;
+        private Vat _vat;
+
+		public string Name {
+			set { SetProperty (ref _name, value); }
+			get { return _name; }
+		}
+
+		public double Rate {
+			set { SetProperty (ref _rate, value); }
+			get { return _rate; }
+		}
+		public decimal Amount {
+			set { SetProperty (ref _amount, value); }
+			get { return _amount; }
+		}
+		public Vat Vat {
+			set { SetProperty (ref _vat, value); }
+			get { return _vat; }
+		}
+    }
+}
