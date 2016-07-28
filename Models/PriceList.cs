631ab57514652e00f6967440a90ea396c134b963
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-07-28 11:54:41 +0200

    Re-add missing PriceList.cs class stub

---
+++ Models/PriceList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amica.vNext.Models
+{
+	// TODO this is just a stub
+
+    public class PriceList : BaseModelWithCompanyId
+    {
+        private string _name;
+
+		public string Name {
+			set { SetProperty (ref _name, value); }
+			get { return _name; } 
+		}
+    }
+}
