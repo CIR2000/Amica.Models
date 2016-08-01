95fdf0dc35f953767debeffc856737402f64c798
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-08-01 14:51:01 +0200

    Change package ID to Amica.Models due to a NuGet conflict

--- Models/Properties/AssemblyInfo.cs
+++ Models/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Models")]
+[assembly: AssemblyTitle("Amica.Models")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CIR 2000")]
