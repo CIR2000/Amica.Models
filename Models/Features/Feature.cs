4b25a9e3d49907e6381179f5535c742ff800fad9
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-01-09 15:11:42 +0100

    Add Feature class

---
+++ Models/Features/Feature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Amica.Models.Features
+{
+    class Feature
+    {
+        public string Name { get; internal set; }
+        public string DisplayName { get; internal set; }
+        public string Tag { get; internal set; }
+        public List<string> Plans { get; internal set; }
+    }
+}
