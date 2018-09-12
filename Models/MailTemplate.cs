beb13b9bc5e479aae8a985d883afb4c15d62990d
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-09-12 09:12:28 +0200

    Add MailSettings.Id property

--- Models/MailTemplate.cs
+++ Models/MailTemplate.cs
@@ -9,6 +9,20 @@ namespace Amica.Models
     public class MailTemplate : ObservableObject
     {
         string _name, _username, _password, _subject, _body, _fromName, _fromAddress;
+        int _id;
+
+        /// <summary>
+        /// Gets or sets the unique id.
+        /// </summary>
+        /// <value>
+        /// The unique id.
+        /// </value>
+        public int Id
+        {
+            set { SetProperty(ref _id, value); }
+            get { return _id; }
+        }
+
         /// <summary>
         /// Gets or sets the tempalte name.
         /// </summary>
@@ -16,10 +30,11 @@ namespace Amica.Models
         /// The template name.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateName), ResourceType = typeof(Resources.Properties))]
-        public string Name {
-			set { SetProperty (ref _name, value); }
-			get { return _name; } 
-		}
+        public string Name
+        {
+            set { SetProperty(ref _name, value); }
+            get { return _name; }
+        }
         /// <summary>
         /// Gets or sets the username.
         /// </summary>
@@ -27,10 +42,11 @@ namespace Amica.Models
         /// The username.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateSmptUsername), ResourceType = typeof(Resources.Properties))]
-        public string SmtpUsername {
-			set { SetProperty (ref _username, value); }
-			get { return _username; } 
-		}
+        public string SmtpUsername
+        {
+            set { SetProperty(ref _username, value); }
+            get { return _username; }
+        }
         /// <summary>
         /// Gets or sets the password.
         /// </summary>
@@ -38,10 +54,11 @@ namespace Amica.Models
         /// The password.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateSmptPassword), ResourceType = typeof(Resources.Properties))]
-        public string SmtpPassword {
-			set { SetProperty (ref _password, value); }
-			get { return _password; } 
-		}
+        public string SmtpPassword
+        {
+            set { SetProperty(ref _password, value); }
+            get { return _password; }
+        }
         /// <summary>
         /// Gets or sets from name.
         /// </summary>
@@ -49,10 +66,11 @@ namespace Amica.Models
         /// From name.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateFromName), ResourceType = typeof(Resources.Properties))]
-        public string FromName {
-			set { SetProperty (ref _fromName, value); }
-			get { return _fromName; } 
-		}
+        public string FromName
+        {
+            set { SetProperty(ref _fromName, value); }
+            get { return _fromName; }
+        }
         /// <summary>
         /// Gets or sets from address.
         /// </summary>
@@ -60,10 +78,11 @@ namespace Amica.Models
         /// From address.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateFromAddress), ResourceType = typeof(Resources.Properties))]
-        public string FromAddress {
-			set { SetProperty (ref _fromAddress, value); }
-			get { return _fromAddress; } 
-		}
+        public string FromAddress
+        {
+            set { SetProperty(ref _fromAddress, value); }
+            get { return _fromAddress; }
+        }
         /// <summary>
         /// Gets or sets the subject.
         /// </summary>
@@ -71,10 +90,11 @@ namespace Amica.Models
         /// The subject.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateMessageSubject), ResourceType = typeof(Resources.Properties))]
-        public string MessageSubject {
-			set { SetProperty (ref _subject, value); }
-			get { return _subject; } 
-		}
+        public string MessageSubject
+        {
+            set { SetProperty(ref _subject, value); }
+            get { return _subject; }
+        }
         /// <summary>
         /// Gets or sets the message body.
         /// </summary>
@@ -82,9 +102,10 @@ namespace Amica.Models
         /// The message body.
         /// </value>
         [Display(Name = nameof(Resources.Properties.TemplateMessageBody), ResourceType = typeof(Resources.Properties))]
-        public string MessageBody {
-			set { SetProperty (ref _body, value); }
-			get { return _body; } 
-		}
+        public string MessageBody
+        {
+            set { SetProperty(ref _body, value); }
+            get { return _body; }
+        }
     }
 }
