6d693a2a53dffc2dc745cbf24958eabd088eded0
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-10-30 09:57:49 +0100

    Add UnitOfMeasureHelpers.UnitOfMeasures collection<string>

---
+++ Models/UnitOfMeasureHelper.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Amica.Models
+{
+    /// <summary>
+    /// Currency helpers
+    /// <summary>
+    public class UnitOfMeasureHelper
+    {
+        /// <summary>
+        /// Known unit of measures
+        ///
+        public static readonly ReadOnlyCollection<string> UnitOfMeasures =
+            new ReadOnlyCollection<string>(new List<string> { "n", "m", "kg", "l", "g", "cm" });
+    }
+}
