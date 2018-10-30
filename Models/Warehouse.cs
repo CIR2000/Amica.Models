94355403392b9ba23ca3dc12c3c9a47c7334d742
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-10-30 10:01:18 +0100

    Warehouse now only has Name property
    Closes CIR2000/Amica.WebApp#31

--- Models/Warehouse.cs
+++ Models/Warehouse.cs
@@ -3,30 +3,11 @@
     public class Warehouse : BaseModelWithCompanyId
     {
         private string _name;
-        private string _notes;
-        private Address _address;
-
-        public Warehouse()
-        {
-            _address = new Address();
-        }
 
         public string Name
         {
             set { SetProperty(ref _name, value); }
             get { return _name; }
-        }
-
-        public string Notes
-        {
-            set { SetProperty(ref _notes, value); }
-            get { return _notes; }
-        }
-
-        public Address Address
-        {
-            set { SetProperty(ref _address, value); }
-            get { return _address; }
         }
     }
 }
