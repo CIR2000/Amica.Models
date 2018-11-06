585497313f934b8e93bce2530d49bffa1a354418
Nicola Iarocci <nicola@nicolaiarocci.com>
2018-11-06 14:40:20 +0100

    New: ProductCategory class. Closes #14

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Amica.Models
+{
+    /// <summary>
+    /// Product category.
+    /// </summary>
+    /// <seealso cref="Amica.Models.BaseModelWithCompanyId" />
+    public class ProductCategory : BaseModelWithCompanyId
+    {
+        private string _name;
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+    }
+}
